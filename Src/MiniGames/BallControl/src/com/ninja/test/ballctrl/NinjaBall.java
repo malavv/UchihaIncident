920da00f924d625116a5b7e67248ac8c9cf2207c
elxeltor <el.xeltor@gmail.com>
2011-05-13 01:08:27 -0400

    ajout des classes NinjaBall et Wall qui sont tout deux des "collidable" soit, objets collisionables. je prévois créer une interface movable pour le objets qui ont du mouvement dans le cas ou on voudrait d'autre choses que la NinjaBall qui puisse bouger, comme des ennemies ou des éléments du décor.

---
+++ Src/MiniGames/BallControl/src/com/ninja/test/ballctrl/NinjaBall.java
@@ -0,0 +1,10 @@
+package com.ninja.test.ballctrl;
+
+public class NinjaBall extends Collidable {
+
+	NinjaBall(float aPosX, float aPosY, float aElasticity) {
+		super(aPosX, aPosY, aElasticity);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Src/MiniGames/BallControl/src/com/ninja/test/ballctrl/Wall.java
@@ -0,0 +1,10 @@
+package com.ninja.test.ballctrl;
+
+public class Wall extends Collidable {
+
+	Wall(float aPosX, float aPosY, float aElasticity) {
+		super(aPosX, aPosY, aElasticity);
+		// TODO Auto-generated constructor stub
+	}
+
+}
