c3a55abd301a0b496bb613df268da8dac1930070
elxeltor <el.xeltor@gmail.com>
2012-01-06 18:33:29 -0500

    les shuriken ne se replacent plus sur les obstacles, par contre, 2 shurikens peuvent être au même endroit, ce qui ne me gêne pas... ce qui me gêne, c'est qu'on ne fasse pas la différence entre 1 et 2 shuriken à la même place... j'essaierais de fixer ça plus tard

--- Src/MiniGames/BallControl/src/com/ninja/test/ballctrl/Coin.java
+++ Src/MiniGames/BallControl/src/com/ninja/test/ballctrl/Coin.java
@@ -17,15 +17,16 @@ public class Coin extends Collidable {
 	
 	public void replace(int Xmax, int Ymax, Iterator<Collidable> obstacles) {
 		Collidable c;
+		int offset = Collidable.getOffset();
 		boolean retry;
 		do {
-			x = (int) (Math.random() * (Global.xDiv-2)) * Collidable.getOffset() + Collidable.getOffset();
-			y = (int) (Math.random() * (Global.yDiv-2)) * Collidable.getOffset() + Collidable.getOffset();
+			x = (int) (Math.random() * (Global.xDiv-2)) * offset + offset;
+			y = (int) (Math.random() * (Global.yDiv-2)) * offset + offset;
 			c = new Collidable(x, y, 1);
 			retry = false;
 			for(Iterator<Collidable> i = obstacles; i.hasNext();){
 				Collidable it = i.next();
-				if(it.equals(c)){
+				if(it.getX() == c.getX()-offset/2 && it.getY() == c.getY()-offset/2){
 					Log.d("DEBUG", "EQUALITY");
 					retry = true;
 				}
